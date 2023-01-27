1d8ac043534a753ef698708c637ba2bf885e4392
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2023-01-27 15:00:45 -0300

    gg

--- www.ligadasnovinhas.com.py
+++ www.ligadasnovinhas.com.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 # Fazer uma solicitação à página da web
-url = 'https://www.ligadasnovinhas.com/cunhada-nua-exibindo-os-peitinhos-e-a-xoxota-gostosa/'
+url = 'https://www.ligadasnovinhas.com/fotos-de-buceta-rosinha-da-gordinha-gostosa/'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
